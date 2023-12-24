510876c379e620abe493636b274257f093f91c48
Shree Mangalam <sshreemangalam@gmail.com>
2023-12-24 23:51:43 +0530

    update

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+const isDev = app.settings.env === "development";
+const URL = isDev ? "http://localhost:3000" : "https://easydraw.vercel.app";
+app.use(cors({ origin: URL }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: "http://localhost:3000",
+  cors: URL,
 });
 
 io.on("connection", (socket) => {
   console.log("Server connected");
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
+
+  socket.on("drawLine", (arg) => {
+    socket.broadcast.emit("drawLine", arg);
+  });
+
+  socket.on("changeConfig", (arg) => {
+    socket.broadcast.emit("changeConfig", arg);
+  });
 });
 
 httpServer.listen(5000);
